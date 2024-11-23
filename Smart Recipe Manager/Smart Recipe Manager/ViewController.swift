31c87f4b2331a804a9ee304ca4b5d0433faba160
Gaurvv08 <gaurvvsingh08@gmail.com>
2024-11-23 11:53:58 +0530

    Add files via upload

---
+++ Smart Recipe Manager/Smart Recipe Manager/ViewController.swift
@@ -0,0 +1,44 @@
+//
+//  ViewController.swift
+//  Smart Recipe Manager
+//
+//  Created by student-2 on 23/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var image: UILabel!
+    
+    @IBOutlet weak var calorie: UILabel!
+    
+    @IBOutlet weak var preparationtime: UILabel!
+    
+    
+    @IBOutlet weak var recipe: UILabel!
+    
+    @IBOutlet weak var dish: UILabel!
+    
+    var myindex = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let selectedrecipe = item[myindex]
+        
+        dish.text = selectedrecipe.dish
+            
+            image.text = selectedrecipe.image
+        calorie.text = selectedrecipe.caloriecount
+        preparationtime.text = selectedrecipe.preparationtime
+        recipe.text = selectedrecipe.recipe
+        
+           
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
+
